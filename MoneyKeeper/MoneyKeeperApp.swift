31ddc52b7703ab0f1226341b33019967d5b753d9
Nikita Yashin <n.yashin88@icloud.com>
2022-08-14 12:09:17 +0300

    Initial Commit

---
+++ MoneyKeeper/MoneyKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyKeeperApp.swift
+//  MoneyKeeper
+//
+//  Created by Nikita Yashin on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
